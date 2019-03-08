f3a4cccf338d8c4d5ba9451ffa9ed06ec5ddcc87
Steve Albonico <steve@albonico.net>
2019-03-08 10:06:26 -0800

    added courses/enrollments migrations

--- db/migrate/20190308170526_create_courses.rb
+++ db/migrate/20190308170526_create_courses.rb
@@ -1,6 +1,11 @@
 class CreateCourses < ActiveRecord::Migration[5.2]
   def change
     create_table :courses do |t|
+    	t.string :name
+    	t.string :teacher
+    	t.integer :lecture_length
+    	t.string :time_of_day
+    	t.string :description
     end
   end
 end
--- db/migrate/20190308170541_create_enrollments.rb
+++ db/migrate/20190308170541_create_enrollments.rb
@@ -1,6 +1,9 @@
 class CreateEnrollments < ActiveRecord::Migration[5.2]
   def change
     create_table :enrollments do |t|
+    	t.integer :course_id
+    	t.integer :user_id
+    	t.string :course_type
     end
   end
 end
