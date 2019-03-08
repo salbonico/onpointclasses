10316aa508bc777242838205b38ddaf28a250026
Steve Albonico <steve@albonico.net>
2019-03-07 16:56:22 -0800

    Userlogin path cleanup

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -20,19 +20,22 @@ class SessionsController < ApplicationController
 
 	def destroy
 		session.delete :user_id
+		redirect_to '/login'
 	end
 
     def socialcreate
-    	
-    @user = User.find_or_create_by(uid: auth['uid']) do |u|
-      u.name = auth['info']['name']
-      u.email = auth['info']['email']
-      u.password = auth['info']['email']
-      u.image = auth['info']['image']
-    end
-   
-    session[:user_id] = @user.id
-    redirect_to '/home'
+    	if session[:user_id] != [] && session[:user_id] != nil
+    		redirect_to '/home'
+    	else
+		    @user = User.find_or_create_by(uid: auth['uid']) do |u|
+			    u.name = auth['info']['name']
+			    u.email = auth['info']['email']
+			    u.password = auth['info']['email']
+			    u.image = auth['info']['image']
+			end
+		    session[:user_id] = @user.id
+		    redirect_to '/home'
+    	end
     end
 
 private
